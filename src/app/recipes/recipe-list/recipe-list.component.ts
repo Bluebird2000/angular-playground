d590d5e9a1f0187c5de5e7e96e61cfaec3bcc6ef
Bluebird2000 <ahmad.saka@photizzo.com>
2019-05-19 14:19:49 +0100

    refactor code base to load recipe detail dynamically

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from './../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
