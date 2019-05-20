3c8e0d1d523e9f8617f8af0e8195bbaff00b6dc9
Bluebird2000 <ahmad.saka@photizzo.com>
2019-05-20 10:32:42 +0100

    using in memory to add recipe to list with dynamic rendering

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,10 @@ import { Recipe } from './../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Recipe', 'This is simply another recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A delecious Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/10/13/05/06/egg-wrapped-fried-rice-1736801__340.jpg'),
+    new Recipe('Another Recipe', 'This is simply another recipe', 'https://cdn.pixabay.com/photo/2015/08/26/10/58/the-pork-fried-rice-made-908333__340.jpg'),
+    new Recipe('yummy Recipe', 'This is simply yummy recipe', 'https://cdn.pixabay.com/photo/2017/07/16/11/57/fried-2509089_960_720.jpg'),
+    new Recipe('elegant recipe', 'This is simply yummy recipe', 'https://cdn.pixabay.com/photo/2016/10/23/09/37/restaurant-1762493__340.jpg'),
   ]
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
